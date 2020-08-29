14c834019690f24e626d2eb1ee711b371dca4bb8
Boganowskiy Andrey <boganowskiy@gmail.com>
2020-08-30 02:40:09 +0300

    Updates

---
+++ precache-manifest.78a5d3293c89b2b0cef9277b0c38b6d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0971e7c1f379602ef46d1cb09a1145d",
+    "url": "/deploytest/index.html"
+  },
+  {
+    "revision": "a3467a8f77ca5d9969e0",
+    "url": "/deploytest/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "79646b79c9aa69aada7b",
+    "url": "/deploytest/static/js/2.4fe1e23d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploytest/static/js/2.4fe1e23d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3467a8f77ca5d9969e0",
+    "url": "/deploytest/static/js/main.b12dcbb2.chunk.js"
+  },
+  {
+    "revision": "fc7d63cd49fc6dc8df60",
+    "url": "/deploytest/static/js/runtime-main.c61cbb4d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploytest/static/media/logo.5d5d9eef.svg"
+  }
+]);
